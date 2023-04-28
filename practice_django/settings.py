dc1639390273321c6a37860458a4552272eb415d
finking <finking@list.ru>
2023-04-28 14:10:06 +0300

    Fix settings

--- practice_django/settings.py
+++ practice_django/settings.py
@@ -29,7 +29,6 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-# SECRET_KEY = 'django-insecure-0x=ws+()irr_i7-y!)917z=q=os4lv+8z5v8nv1g+0xw=e@tq^'
 SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
