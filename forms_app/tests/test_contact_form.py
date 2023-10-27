855ec3ed37da2d831b1fca1454731d71670d1f2d
finking <finking@list.ru>
2023-10-27 16:17:38 +0300

    Added test for some fields of form. Les_till_4/30

--- forms_app/tests/test_contact_form.py
+++ forms_app/tests/test_contact_form.py
@@ -3,35 +3,112 @@ from ..forms import ContactForm
 import datetime
 import pytest
 
+# Переменные для проверки длины заполенных символов в поле
+max_length_gt_100 = 'a' * 101
+max_length_gt_500 = 'a' * 501
 
 #Что тестируем и данные
-# TODO переделать на автоматические даты.
+# # TODO переделать на автоматические даты.
+# @pytest.mark.parametrize(
+#     'date_creation, subject, message, sender, cc_myself, validity',
+#     [
+#         # Рабочий вариант
+#         ('2023-10-20', 'django lessons', 'I want to learn Django.', 'mail@mail.ru', 'cc_myself', True),
+#
+#         # Дата меньше текущей
+#         ('2023-10-08', 'django lessons', 'Hi! I want to lern DjANGO', 'django@mail.ru', 'cc_myself', False),
+#
+#         # Дата старше текущей
+#         ('2023-10-21', 'django lessons', 'Hi! I want to lern DjANGO', 'django@mail.ru', 'cc_myself', False),
+#
+#         # в емейл нет собаки
+#         ('2022-02-09', 'django lessons', 'Hi! I want to lern DjANGO', 'djangomail.ru', 'cc_myself', False),
+#     ]
+# )
+# def test_valid_contact_form(date_creation, subject, message, sender, cc_myself, validity):
+#     form = ContactForm(data={
+#         'date_creation': date_creation,
+#         'subject': subject,
+#         'message': message,
+#         'sender': sender,
+#         'cc_myself': cc_myself
+#     })
+#
+#     # f = form.errors()
+#     # print(f)
+#
+#     assert form.is_valid() is validity
+
+
 @pytest.mark.parametrize(
-    'date_creation, subject, message, sender, cc_myself, validity',
+    'date_creation, valid_date',
     [
-        # Рабочий вариант
-        ('2023-10-20', 'django lessons', 'I want to learn Django.', 'mail@mail.ru', 'cc_myself', True),
-        
-        # Дата меньше текущей
-        ('2023-10-08', 'django lessons', 'Hi! I want to lern DjANGO', 'django@mail.ru', 'cc_myself', False),
-    
-        # Дата старше текущей
-        ('2023-10-21', 'django lessons', 'Hi! I want to lern DjANGO', 'django@mail.ru', 'cc_myself', False),
-    
-        # в емейл нет собаки
-        ('2022-02-09', 'django lessons', 'Hi! I want to lern DjANGO', 'djangomail.ru', 'cc_myself', False),
+        # Сегодняшняя дата
+        (datetime.datetime.today(), True),
+        # Дата младше текущей
+        ('2023-10-26', False),
+        # Дата позже текущей
+        (datetime.datetime.today() + datetime.timedelta(days=1), False),
+        # Проверка пустой строки
+        ("", False)
     ]
 )
-def test_valid_contact_form(date_creation, subject, message, sender, cc_myself, validity):
+
+@pytest.mark.parametrize(
+    'subject, valid_subject',
+    [
+        # Обычный заголовок
+        ('Header', True),
+        # Заголовк превышающий лимит в 100 символов
+        (max_length_gt_100, False),
+        ('', False),
+    ]
+)
+
+@pytest.mark.parametrize(
+    'message, valid_message',
+    [
+        # Обычный текст
+        ('This is some message', True),
+        # Текст превышающий лимит в 500 символов
+        (max_length_gt_500, False),
+        ('', False),
+    ]
+)
+
+@pytest.mark.parametrize(
+    'sender, valid_sender',
+    [
+        # Валидный email
+        ('tt@mail.ru', True),
+        # Неверный email
+        ('tt', False),
+        # Обязательное поле
+        ('', False),
+    ]
+)
+
+@pytest.mark.parametrize(
+    'cc_myself, valid_cc_myself',
+    [
+        ('cc_myself', True), # Не понятно, почему для чек-бокса указываем именно имя этого поля.
+        # (),
+        # (),
+    ]
+)
+
+
+def test_contact_form(date_creation, subject, message, sender, cc_myself,
+                      valid_date, valid_subject, valid_message, valid_sender, valid_cc_myself):
     form = ContactForm(data={
-        'date_creation': date_creation,
-        'subject': subject,
-        'message': message,
-        'sender': sender,
-        'cc_myself': cc_myself
+        "date_creation": date_creation,
+        "subject": subject,
+        "message": message,
+        "sender": sender,
+        "cc_myself": cc_myself
     })
     
-    # f = form.errors()
-    # print(f)
+    f = form.errors.as_data()
+    print(f)
     
-    assert form.is_valid() is validity
+    assert form.is_valid() is (valid_date and valid_subject and valid_message and valid_sender and valid_cc_myself)
